55b894ef0af3497535c22e63e0b133c1a0fa28ab
Manuell00 <manuell.caselli@gmail.com>
2023-07-26 12:15:12 +0200

    modifiche edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -16,72 +16,74 @@
                 @csrf
                 @method("POST")
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="project_name">project_name</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="project_name"><b>project_name :</b></label>
                         <br>
-                        <input class="text-center" type="text" name="project_name" value="{{$project -> project_name}}">
+                        <input class="text-center form-control" type="text" name="project_name" value="{{$project -> project_name}}">
                     </div>
                 </div>
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="description">description</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="description"><b>description :</b></label>
                         <br>
-                        <input class="text-center" type="text" name="description" value="{{$project -> description}}">
-                    </div>
-                </div>
-
-
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="start_date">start_date</label>
-                        <br>
-                        <input class="text-center" type="date" name="start_date" value="{{$project -> start_date}}">
+                        <textarea class="form-control description-textarea" name="description">{{ $project->description }}</textarea>
                     </div>
                 </div>
 
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="end_date">end_date</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-3">
+                        <label for="start_date"><b>start_date :</b></label>
                         <br>
-                        <input class="text-center" type="date" name="end_date" value="{{$project -> end_date}}">
+                        <input class="text-center form-control " type="date" name="start_date" value="{{$project -> start_date}}">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="end_date"><b>end_date :</b></label>
+                        <br>
+                        <input class="text-center form-control" type="date" name="end_date" value="{{$project -> end_date}}">
                     </div>
                 </div>
 
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="status">status</label>
+                <div class="row justify-content-center my-4">
+                   
+                </div>
+
+
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="status"><b>status :</b></label>
                         <br>
-                        <input class="text-center" type="text" name="status" value="{{$project -> status}}">
+                        <input class="text-center form-control" type="text" name="status" value="{{$project -> status}}">
                     </div>
                 </div>
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="budget">budget</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="budget"><b>budget :</b></label>
                         <br>
-                        <input class="text-center" type="number" name="budget" value="{{$project -> budget}}">
+                        <input class="text-center form-control" type="number" name="budget" value="{{$project -> budget}}">
                     </div>
                 </div>
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="progress">progress</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="progress"><b>progress :</b></label>
                         <br>
-                        <input class="text-center" type="number" name="progress" value="{{$project -> progress}}">
+                        <input class="text-center form-control" type="number" name="progress" value="{{$project -> progress}}">
                     </div>
                 </div>
 
-                <div class="row justify-content-center my-3">
-                    <div class="col-3">
-                        <label for="type_id">Type</label>
+                <div class="row justify-content-center my-4">
+                    <div class="col-md-6">
+                        <label class="my-2" for="type_id"><b>Type :</b></label>
                         <br>
                         <select name="type_id" id="type_id">
                          @foreach ($types as $type)
-                            <option  class="text-center" value="{{$type -> id}}">
+                            <option  class="text-center form-control" value="{{$type -> id}}">
                                 <div>{{$type-> type_name}}</div>
                             </option>
                          @endforeach
@@ -91,7 +93,7 @@
                 </div>
 
                 
-                <div class="row justify-content-center my-3">
+                <div class="row justify-content-center my-4">
                     <div class="col-3">
                         <input class="btn btn-primary" type="submit">
                     </div>
